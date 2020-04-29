9bd79588afe56627384bf6c9eb0d465ede3c94f0
Ryan W. Auger <35308806+rwauger@users.noreply.github.com>
2020-04-29 18:33:01 -0400

    Update sde1_sp2020.ml

--- sde1_sp2020.ml
+++ sde1_sp2020.ml
@@ -41,3 +41,21 @@ let rec outer = function(v1, v2, pos)->
 
 let hopTrainAstate = function(astate)->
     outer(astate, astate, 0);;
+    
+let rec sum = function (v1, v2) ->
+if v1 = [] then
+[]
+else
+List.hd v1 +. List.hd v2 :: sum (List.tl v1, List.tl v2);;
+
+let rec sumMat = function (v1, v2) ->
+    if v2 = [] then
+        v1
+    else
+        sum (List.hd v1, List.hd v2) :: sumMat (List.tl v1, List.tl v2);;
+
+let rec hopTrain = function (allStates) ->
+    if allStates = [] then
+        []
+    else
+        sumMat (hopTrainAstate (List.hd allStates), hopTrain (List.tl allStates));;
